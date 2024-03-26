aaa123e4feec378538e93ed3abe9e45e68213878
abdulsamed koçyiğit <abdulsamed.kcygt@gmail.com>
2024-03-26 17:13:06 +0300

    userController has been created for crud operation

---
+++ controllers/userController.go
@@ -0,0 +1,103 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	var user models.User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+
+	// New user adding to database
+	err = models.AddUser(user)
+	if err != nil {
+		http.Error(w, "Failed to add user", http.StatusInternalServerError)
+		return
+	}
+
+	// Success Response
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(user)
+}
+func getUser(w http.ResponseWriter, r *http.Request) {
+	// get the id from req.params
+	userID := r.URL.Query().Get("id")
+	if userID == "" {
+		http.Error(w, "User ID is required", http.StatusBadRequest)
+		return
+	}
+
+	// Get the user from DB with id
+	user, err := models.GetUser(userID)
+	if err != nil {
+		http.Error(w, "User not found", http.StatusNotFound)
+		return
+	}
+
+	// user response
+	json.NewEncoder(w).Encode(user)
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	// Get the all user from DB
+	users, err := models.GetUsers()
+	if err != nil {
+		http.Error(w, "Failed to fetch users", http.StatusInternalServerError)
+		return
+	}
+
+	// response user
+	json.NewEncoder(w).Encode(users)
+}
+
+func updateUser(w http.ResponseWriter, r *http.Request) {
+	// Get the id from req.params
+	userID := r.URL.Query().Get("id")
+	if userID == "" {
+		http.Error(w, "User ID is required", http.StatusBadRequest)
+		return
+	}
+
+	var updatedUser models.User
+	err := json.NewDecoder(r.Body).Decode(&updatedUser)
+	if err != nil {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+
+	// Update User
+	err = models.UpdateUser(userID, updatedUser)
+	if err != nil {
+		http.Error(w, "Failed to update user", http.StatusInternalServerError)
+		return
+	}
+
+	// Success Response
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(updatedUser)
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	// Get the id from req.params
+	userID := r.URL.Query().Get("id")
+	if userID == "" {
+		http.Error(w, "User ID is required", http.StatusBadRequest)
+		return
+	}
+
+	// Delete user
+	err := models.DeleteUser(userID)
+	if err != nil {
+		http.Error(w, "Failed to delete user", http.StatusInternalServerError)
+		return
+	}
+
+	// Success response
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("User deleted successfully"))
+}
