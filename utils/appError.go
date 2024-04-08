f5bb19e420a73d844d98158e8ad04bcc075c7901
abdulsamed koçyiğit <abdulsamed.kcygt@gmail.com>
2024-04-08 10:22:37 +0300

    new error handling func has been defined

--- utils/appError.go
+++ utils/appError.go
@@ -1,5 +1,11 @@
 package utils
 
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+)
+
 // AppError is a custom error type
 type AppError struct {
 	Message       string
@@ -21,4 +27,21 @@ func NewAppError(message string, statusCode int) *AppError {
 		Status:        status,
 		IsOperational: true,
 	}
+}
+
+func RespondWithError(w http.ResponseWriter, statusCode int, message string) {
+	w.WriteHeader(statusCode)
+	response := map[string]interface{}{"error": message}
+	json.NewEncoder(w).Encode(response)
+}
+
+func RespondWithJSON(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(data)
+}
+
+func IsProductionEnvironment() bool {
+	environment := os.Getenv("APP_ENV")
+	return environment == "production"
 }
